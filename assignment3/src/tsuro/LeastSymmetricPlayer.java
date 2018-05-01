8e8fa7b40d61f14ca2837b1d7d765b5aad447ff4
rohitrastogi <rohitrastogi2019@u.northwestern.edu>
2018-05-01 15:53:02 -0500

    LeastSymmetricalPlayer implemented

--- assignment3/src/tsuro/LeastSymmetricPlayer.java
+++ assignment3/src/tsuro/LeastSymmetricPlayer.java
@@ -1,7 +1,7 @@
 package tsuro;
 
-import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class LeastSymmetricPlayer extends Player {
@@ -15,9 +15,26 @@ public class LeastSymmetricPlayer extends Player {
 	public Tile playTurn(Board b, List<Tile> hand, int tilesRemaining) {
 		// make a priority queue for the tiles, based on symmetry 
 		PriorityQueue<Tile> queue = new PriorityQueue<Tile>(3, new LeastSymmetricalComparison());
+		for (Tile t : hand) {
+			queue.add(t); 
+		}
 		
-		// TODO go through queue and try rotations until one is legal 
-		return hand.get(0); 
+		// go through queue and try rotations until one is legal 
+		Tile toPlay; 
+		while(queue.peek() != null) {
+			toPlay = queue.poll(); 
+			
+			for (int i = 0; i < Tile.NUM_SIDES; i++) {
+				toPlay = toPlay.rotate(i); 
+				if (ServerUtils.isLegalPlay(Server.server.getSPlayer(this), b, toPlay)) {
+					return toPlay; 
+				}
+			}
+		}
+		
+		// Hypothetically by this point no hand should ever have an unplayable tile 
+		// Since isLegalPlay checks other possible moves 
+		throw new RuntimeException("Somehow playTurn for player " + this.getName() + " has a hand with no playable tile."); 
 	}
 }
 
